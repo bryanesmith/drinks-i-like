682b927e0b20c34f1224fa6167551ed1bb1ead1e
Bryan Smith <besmit@umich.edu>
2012-05-22 10:10:53 -0400

    Changed initial example drink description to ridiculous Schwarzenegger quote.

--- install/init.sql
+++ install/init.sql
@@ -7,9 +7,10 @@ CREATE TABLE `drink` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(128) NOT NULL,
   `description` text NOT NULL,
-  PRIMARY KEY( `id` )
+  PRIMARY KEY( `id` ),
+  UNIQUE ( `title` )
 );
 
 -- Test data.
-INSERT INTO `drink`( `title`, `description` ) VALUES ( 'milk', '"Baby mammals drink milk, and you sir, are a baby mammal." - Mark Rippetoe' );
+INSERT INTO `drink`( `title`, `description` ) VALUES ( 'milk', '"Milk is for babies. When you grow up you have to drink beer." - Arnold Schwarzenegger' );
 
